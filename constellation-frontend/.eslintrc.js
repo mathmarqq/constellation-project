2d2c505807dcfbc68ab1b973b09c67048fda309e
Matheus Marques <matheusmarques312@gmail.com>
2021-12-20 17:57:20 -0300

    Adding eslint

---
+++ constellation-frontend/.eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/prettier',
+        'plugin:storybook/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+    plugins: ['react-hooks', 'react', '@typescript-eslint'],
+    rules: {
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'react/jsx-filename-extension': [
+            'warn',
+            {
+                extensions: ['.tsx'],
+            },
+        ],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'react/prop-types': 'off',
+        'react/function-component-definition': 'off',
+        'import/prefer-default-export': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'import/no-extraneous-dependencies': [
+            'error',
+            { devDependencies: ['**/*.test.ts', '**/*.test.tsx', '**/*.stories.tsx'] },
+        ],
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        '@typescript-eslint/no-empty-function': 'off',
+    },
+}
